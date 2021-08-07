ea94c3f8157fb1fe1881a11210530acd03b35bc1
BrydenIsNotSmart <Brydenwcgames@gmail.com>
2021-08-06 19:22:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 ﻿
         module.exports = {
             bot: {
-                token: "NzU5NDEyODA3MDMxNzE3OTA4.X29ISg.8dxw4DITLPvBUJvzLZsgpUR7h-M",
-                prefix: "b!",
-                owners: ["529815278456930314"],
-                mongourl: "mongodb+srv://Bryden:Bry277272@cluster0.tlnrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+                token: "",
+                prefix: "",
+                owners: [""],
+                mongourl: "",
                 servers: {
-                    token: "NzY5NTY1OTc2NzAxNDM1OTE1.X5Q4Kg.zxRgRm63RBmO69u_OGwAuN4Vkvw",
-                    prefix: "?"
+                    token: "",
+                    prefix: ""
                 }
             },
         
             website: {
-                callback: "https://www.blistings.xyz/callback",
-                secret: "1zHRyFiFKWBssFaJGo7ZnzA7U3Z5Z6il",
-                clientID: "759412807031717908", 
+                callback: "https://your.domain.com/callback",
+                secret: "", // bot client secrect
+                clientID: "", // bot id
                 tags: [ "Moderation", "Fun", "Minecraft","Economy","Guard","Anime","Invite","Music","Logging", "Web Dashboard", "Reddit", "Youtube", "Twitch", "Crypto", "Leveling", "Game", "Roleplay", "Utility", "Turkish" ],
                 languages: [
                     { flag: 'gb', code: 'en', name: 'English' },
@@ -120,42 +120,42 @@
             },
         
             server: {
-                id: "852580838921994240",
+                id: "",
                 invite: "https://discord.gg/CqgCUE2Dsf",
                 roles: {
-                    administrator: "852580838921994245",
-                    moderator: "852580838921994244",
+                    administrator: "",
+                    moderator: "",
                     profile: {
                         sitecreator : "",
                         booster: "",
                         sponsor: "",
-                        supporter: "867445228658360330",
-                        partnerRole: "853416155272577084"
+                        supporter: "",
+                        partnerRole: ""
                     },
                     codeshare: {
-                        javascript: "868972998659543110",
-                        html: "868972998659543110",
-                        substructure: "868972998659543110",
-                        bdfd: "868972998659543110", 
-                        fiveInvite: "868972998659543110",
-                        tenInvite: "868972998659543110",
-                        fifteenInvite: "868972998659543110",
-                        twentyInvite: "868972998659543110"
+                        javascript: "",
+                        html: "",
+                        substructure: "",
+                        bdfd: "", 
+                        fiveInvite: "",
+                        tenInvite: "",
+                        fifteenInvite: "",
+                        twentyInvite: ""
                     },
                     botlist: {
-                        developer: "854122075400306708",
-                        certified_developer: "867146438496878612",
-                        bot: "852580838921994243",
-                        certified_bot: "867146597310005258",
+                        developer: "",
+                        certified_developer: "",
+                        bot: "",
+                        certified_bot: "",
                     }
                 },
                 channels: {
-                    codelog: "866509296254058536",
-                    login: "866509332925120522",
-                    webstatus: "871902272420974652",
-                    uptimelog: "866509296254058536",
-                    botlog: "866509296254058536",
-                    votes: "868973322032005130"
+                    codelog: "",
+                    login: "",
+                    webstatus: "",
+                    uptimelog: "",
+                    botlog: "",
+                    votes: ""
                 }
             }
         
